f84fe0169fa7c024d146b466ccff801e2a9f5464
vlad enchev <vlad.enchev@outlook.com>
2021-01-18 22:50:14 +0200

    2021.01.18

---
+++ C# Advanced/CA04-StacksAndQueues/01.BasicStackOperations/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.BasicStackOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // input
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = input[0];
+            int s = input[1];
+            int x = input[2];
+
+            // output
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
