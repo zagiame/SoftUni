667bf6bd7445d630c9e53302707756761d065b55
vlad enchev <vlad.enchev@outlook.com>
2021-01-21 22:59:25 +0200

    stacks

---
+++ C# Advanced/CA04-StacksAndQueues/08.BalancedParenthesis/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08.BalancedParenthesis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // input
+            string input = Console.ReadLine();
+
+            // calculation
+            var brackets = new Stack<char>();
+            bool isValid = false;
+
+            foreach (char item in input)
+            {
+                if (item == '(' || item == '{' || item == '[')
+                {
+                    brackets.Push(item);
+                }
+
+                else
+                {
+                    if (brackets.Any() == true)
+                    {
+                        bool isFirstValid = item == ')' && brackets.Pop() == '(';
+                        bool isSecondValid = item == '}' && brackets.Pop() == '{';
+                        bool isThirdValid = item == ']' && brackets.Pop() == '[';
+
+                        if (isFirstValid == false && isSecondValid == false && isThirdValid == false)
+                        {
+                            isValid = false;
+                            break;
+                        }
+
+                        else
+                        {
+                            isValid = true;
+                        }
+                    }
+                }
+            }
+
+            // output
+            if (isValid == true)
+            {
+                Console.WriteLine("YES");
+            }
+
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
