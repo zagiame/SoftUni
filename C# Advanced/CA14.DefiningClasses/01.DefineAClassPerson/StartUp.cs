6f2b8099d23893aa71b015c0863fdc422754ea3b
vlad enchev <vlad.enchev@outlook.com>
2021-02-08 13:50:50 +0200

    DefiningClasses

---
+++ C# Advanced/CA14.DefiningClasses/01.DefineAClassPerson/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using DefiningClasses;
+using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Person personOne = new Person();
+            Person personTwo = new Person(25);
+            Person personThree = new Person("Peter", 24);
+        }
+    }
+}
