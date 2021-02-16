d474f52c5a09fedbd995aa2a4f7e5ce4b3a09d2f
vlad enchev <vlad.enchev@outlook.com>
2021-02-16 22:33:44 +0200

    exam preparation

---
+++ C# Advanced/CA21.ExamPreparation-25October2020/Garden/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Garden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // input
+            string[] matrixData = Console.ReadLine().Split();
+            int n = int.Parse(matrixData[0]);
+            int m = int.Parse(matrixData[1]);
+
+            // calculation
+            int[,] matrix = new int[n, m];
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < m; col++)
+                {
+                    matrix[row, col] = 0;
+                }
+            }
+
+            // position to plant
+            string input = string.Empty;
+            var plants = new List<string>();
+
+            while ((input = Console.ReadLine()) != "Bloom Bloom Plow")
+            {
+                string[] inputData = input.Split();
+                int currentRow = int.Parse(inputData[0]);
+                int currentCol = int.Parse(inputData[1]);
+
+                if (IndexIsInsideOfMatrix(currentRow, currentCol, n, m) == true)
+                {
+                    matrix[currentRow, currentCol] = 1;
+                    string flower = $"{currentRow} {currentCol}";
+                    plants.Add(flower);
+                }
+
+                else
+                {
+                    Console.WriteLine("Invalid coordinates.");
+                }
+            }
+
+            // bloom
+
+            foreach (var item in plants)
+            {
+                Bloom(item, matrix);
+            }
+
+
+            // output
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write($"{matrix[row, col]} ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private static void Bloom(string item, int[,] matrix)
+        {
+            string[] rowData = item.Split();
+            int currentRow = int.Parse(rowData[0]);
+            int currentCol = int.Parse(rowData[1]);
+            int n = matrix.GetLength(0);
+            int m = matrix.GetLength(1);
+
+            // bloom up
+            for (int up = 1; up < n; up++)
+            {
+                if (IndexIsInsideOfMatrix(currentRow - up, currentCol, n, m) == true)
+                {
+                    matrix[currentRow - up, currentCol]++;
+                }
+
+                else
+                {
+                    break;
+                }
+
+            }
+
+            // bloom down
+            for (int down = 1; down < n; down++)
+            {
+                if (IndexIsInsideOfMatrix(currentRow + down, currentCol, n, m) == true)
+                {
+                    matrix[currentRow + down, currentCol]++;
+                }
+
+                else
+                {
+                    break;
+                }
+
+            }
+
+            // bloom left
+            for (int left = 1; left < m; left++)
+            {
+                if (IndexIsInsideOfMatrix(currentRow, currentCol - left, n, m) == true)
+                {
+                    matrix[currentRow, currentCol - left]++;
+                }
+
+                else
+                {
+                    break;
+                }
+
+            }
+
+            // bloom right
+            for (int right = 1; right < m; right++)
+            {
+                if (IndexIsInsideOfMatrix(currentRow, currentCol + right, n, m) == true)
+                {
+                    matrix[currentRow, currentCol + right]++;
+                }
+
+                else
+                {
+                    break;
+                }
+
+            }
+
+        }
+
+        private static bool IndexIsInsideOfMatrix(int currentRow, int currentCol, int totalRow, int totalCol)
+        {
+            if (currentRow >= 0 && currentRow < totalRow && currentCol >= 0 && currentCol < totalCol)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ C# Advanced/CA21.ExamPreparation-25October2020/Scheduling/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Scheduling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // input
+            var task = new Stack<int>(Console.ReadLine().Split(", ").Select(int.Parse));
+            var thread = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+            int taskToBeKilled = int.Parse(Console.ReadLine());
+
+            // calculation
+            int killerThread = 0;
+
+            while (task.Count > 0 && thread.Count > 0)
+            {
+                int currentTask = task.Peek();
+                int currentThead = thread.Peek();
+
+                if (currentTask == taskToBeKilled)
+                {
+                    task.Pop();
+                    killerThread = currentThead;
+                    break;
+                }
+
+                else if (currentThead < currentTask)
+                {
+                    thread.Dequeue();
+                }
+
+                else
+                {
+                    task.Pop();
+                    thread.Dequeue();
+                }
+            }
+
+            // output
+            Console.WriteLine($"Thread with value {killerThread} killed task {taskToBeKilled}");
+
+            if (thread.Count > 0)
+            {
+                Console.WriteLine(string.Join(" ", thread));
+            }
+        }
+    }
+}
